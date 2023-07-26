023c6e1747b6a9f22ee59755d53e5c8ee8804102
nobata <ahirururu122@gmail.com>
2023-07-27 00:25:39 +0900

    テンプレートの修正とCSSファイルの追加

--- app/forms.py
+++ app/forms.py
@@ -5,8 +5,9 @@ ORDER_CHOICES = {
     'viewCount': '再生回数の多い順',
     'date': '作成日の新しい順',
     'rating': '評価の高い順',
-    'relevance': '関連性の高い順',
+    'relevance': '関連性が高い順',
 }
+
 
 class KeywordForm(forms.Form):
     keyword = forms.CharField(max_length=100, label='キーワード')
@@ -14,5 +15,7 @@ class KeywordForm(forms.Form):
     viewcount = forms.IntegerField(label='再生回数')
     order = forms.ChoiceField(
         label='並び順', widget=forms.Select, choices=list(ORDER_CHOICES.items()))
-    search_start = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}), label='検索開始日')
-    search_end = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}), label='検索終了日')
+    search_start = forms.DateField(widget=forms.DateInput(
+        attrs={'type': 'date'}), label='検索開始日')
+    search_end = forms.DateField(widget=forms.DateInput(
+        attrs={'type': 'date'}), label='検索終了日')
--- app/views.py
+++ app/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render, redirect
 from django.views.generic import View
+from django.shortcuts import render, redirect
 from apiclient.discovery import build
 from datetime import datetime, timedelta, date
 from django.conf import settings
@@ -8,23 +8,25 @@ import pandas as pd
 
 
 # 環境変数の読み込み
-YOUTUBE_API = build('youtube', 'v3', developerkey=settings.YOUTUBE_API_KEY)
+YOUTUBE_API = build('youtube', 'v3', developerKey=settings.YOUTUBE_API_KEY)
 
 
 # トップページ
 class IndexView(View):
     def get(self, request, *args, **kwargs):
+        # 検索フォーム
         form = KeywordForm(
             request.POST or None,
+            # フォームに初期値を設定
             initial={
-                'items_count': 12,
-                'viewcount': 1000,
-                'order': 'viewCount',
-                'search_start': datetime.today() - timedelta(days=30),
-                'search_end': datetime.today(),
+                'items_count': 12,  # 検索数
+                'viewcount': 1000,  # 再生回数
+                'order': 'viewCount',  # 並び順
+                'search_start': datetime.today() - timedelta(days=30),  # 1ヶ月前
+                'search_end': datetime.today(),  # 本日
             }
         )
 
         return render(request, 'app/index.html', {
-            'form': form,
+            'form': form
         })
